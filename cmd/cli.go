f13f53de3477993e625e36ec1d4ea0e54fe52eb2
cezmunsta <github@incoming-email.co.uk>
2021-02-24 15:42:36 +0000

    Added listConnections and tests

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,22 +1,33 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"os"
+	"reflect"
 	"runtime"
 	"strings"
 
 	vaultApi "github.com/hashicorp/vault/api"
 	"github.com/spf13/cobra"
+
+	"github.com/cezmunsta/ssh_ms/log"
+	vaultHelper "github.com/cezmunsta/ssh_ms/vault"
 )
 
 // EnvBasePath is the parent location used to prefix storage paths
 const EnvBasePath = "HOME"
 
 type cmdFlags struct {
-	List, Verbose, Version bool
-	Show                   string
+	List, Simulate, StoredToken, Verbose, Version bool
+	Addr, Show, Token                             string
 }
+
+//type cmdFlags struct {
+//    Expert, Purge, bool
+//    Comment, StoragePath, User, Write         string
+//}
 
 var (
 	rootCmd = &cobra.Command{
@@ -39,10 +50,24 @@ var (
 		},
 	}
 
+	listCmd = &cobra.Command{
+		Use:   "list [flags]",
+		Short: "List available connections",
+		Long:  "Lookup available connections in Vault and list them",
+		Run: func(cmd *cobra.Command, args []string) {
+			listConnections(getVaultClient())
+		},
+	}
+
 	flags = cmdFlags{}
 
-	// Version can be set with `-ldflags "-X github.com/cezmunsta/ssh_ms/cmd.Version=xxx"`
-	Version = "1.0"
+	/*
+		The following support overrides during builds, which can be done
+		by setting ldflags, e.g.
+
+		`-ldflags "-X github.com/cezmunsta/ssh_ms/cmd.Version=xxx"`
+
+	*/
 
 	// EnvSSHUsername is used to authenticate with SSH
 	EnvSSHUsername = "SSH_MS_USERNAME"
@@ -52,11 +77,95 @@ var (
 
 	// EnvVaultAddr is the default location for Vault
 	EnvVaultAddr = vaultApi.EnvVaultAddress
+
+	// SecretPath is the location used for connection manangement
+	SecretPath = "secret/ssh_ms"
+
+	// Version of the code
+	Version = "1.0"
 )
 
 func init() {
+	rootCmd.AddCommand(
+		listCmd,
+	)
+	rootCmd.PersistentFlags().StringVar(&flags.Addr, "vault-addr", os.Getenv(vaultApi.EnvVaultAddress), "Specify the Vault address")
+	rootCmd.PersistentFlags().StringVar(&flags.Token, "vault-token", os.Getenv(vaultApi.EnvVaultToken), "Specify the Vault token")
+
+	rootCmd.PersistentFlags().BoolVarP(&flags.StoredToken, "stored-token", "", false, "Use a stored token from 'vault login' (overrides --vault-token, auto-enabled when no token is specified)")
 	rootCmd.PersistentFlags().BoolVarP(&flags.Verbose, "verbose", "v", false, "Provide addition output")
+
 	rootCmd.Flags().BoolVarP(&flags.Version, "version", "V", false, "Show the version")
+}
+
+// getVaultClient by authenticating using flags
+func getVaultClient() *vaultApi.Client {
+	env := vaultHelper.UserEnv{Addr: flags.Addr, Token: flags.Token, Simulate: flags.Simulate}
+	return getVaultClientWithEnv(env)
+}
+
+// getVaultClientWithEnv by authenticating using UserEnv
+func getVaultClientWithEnv(env vaultHelper.UserEnv) *vaultApi.Client {
+	if flags.Verbose {
+		log.Debug("Vault Address:", env.Addr)
+		log.Debug("Simulate:", flags.Simulate)
+	}
+	return vaultHelper.Authenticate(env, flags.StoredToken)
+}
+
+// listConnections from Vault
+func listConnections(vc *vaultApi.Client) bool {
+	connections, err := getConnections(vc)
+
+	if err != nil {
+		log.Panic("Unable to list connections:", err)
+	}
+
+	if len(connections) == 0 {
+		fmt.Println("no available connections")
+		return true
+	}
+	for i, s := range connections {
+		m := " "
+		if math.Mod(float64(i), 3) == 0 && i > 0 {
+			m += "\n"
+		}
+		fmt.Print(s, m)
+	}
+	fmt.Println("")
+	return true
+}
+
+// getRawConnection retrieves the secret from Vault
+func getRawConnection(vc *vaultApi.Client, key string) (*vaultApi.Secret, error) {
+	secret, err := vaultHelper.ReadSecret(vc, fmt.Sprintf("%s/%s", SecretPath, key))
+
+	if err != nil || secret == nil {
+		log.Warning("Unable to find connection for:", key)
+		return nil, errors.New("No match found")
+	}
+	return secret, nil
+}
+
+// getConnections from Vault
+func getConnections(vc *vaultApi.Client) ([]string, error) {
+	var connections []string
+	secrets, err := vaultHelper.ListSecrets(vc, SecretPath)
+
+	if err != nil {
+		log.Panic("Unable to get connections:", err)
+	} else if secrets == nil || secrets.Data["keys"] == nil {
+		return nil, errors.New("No data returned")
+	}
+
+	switch reflect.TypeOf(secrets.Data["keys"]).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(secrets.Data["keys"])
+		for i := 0; i < s.Len(); i++ {
+			connections = append(connections, fmt.Sprintf("%s", s.Index(i)))
+		}
+	}
+	return connections, nil
 }
 
 // getVersion information for the application
--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -5,6 +5,14 @@ import (
 	"io/ioutil"
 	"strings"
 	"testing"
+
+	vaultHelper "github.com/cezmunsta/ssh_ms/vault"
+	vaultApi "github.com/hashicorp/vault/api"
+)
+
+var (
+	vc  *vaultApi.Client
+	env = vaultHelper.UserEnv{Addr: "http://127.0.0.1:8200", Token: "myroot", Simulate: false}
 )
 
 func TestExecute(t *testing.T) {
@@ -26,7 +34,28 @@ func TestExecute(t *testing.T) {
 	for _, line := range ver {
 		vb = []byte(strings.Join(line, " "))
 		if string(out) != string(vb) {
-			t.Fatalf("expected: '%s' got '%s'", string(vb), string(out))
+			t.Fatalf("expected: '%s' got: '%s'", string(vb), string(out))
 		}
+	}
+}
+
+func TestGetVaultClient(t *testing.T) {
+	vc = getVaultClientWithEnv(env)
+	if len(vc.Token()) == 0 {
+		t.Fatalf("expected: a non-zero length token got: a zero length token")
+	}
+}
+
+func TestGetConnections(t *testing.T) {
+	if cl, err := getConnections(getVaultClientWithEnv(env)); err != nil {
+		t.Fatalf("expected: a connection list got: '%s'", err)
+	} else {
+		t.Logf("connections found: '%s'", cl)
+	}
+}
+
+func TestGetRawConnection(t *testing.T) {
+	if cn, err := getRawConnection(getVaultClientWithEnv(env), "ceri"); err != nil || cn == nil {
+		t.Fatalf("expected: connection data got: '%v', err '%s'", cn, err)
 	}
 }
