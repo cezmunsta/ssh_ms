975219353c803045740ec81650b8df5c83be5186
cezmunsta <github@incoming-email.co.uk>
2021-02-24 12:07:36 +0000

    Added vault helper

---
+++ vault/helper.go
@@ -0,0 +1,52 @@
+package vault
+
+import (
+	"github.com/hashicorp/vault/api"
+)
+
+// UserEnv contains settings from the ENV
+type UserEnv struct {
+	Addr, Token string
+	Simulate    bool
+}
+
+// DeleteSecret removes a secret from Vault
+func DeleteSecret(c *api.Client, key string) (bool, error) {
+	if _, err := c.Logical().Delete(key); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// ListSecrets reads the list of secrets/data under a path in Vault
+// c : Vault client
+// path : path to secret/data in Vault
+func ListSecrets(c *api.Client, path string) (*api.Secret, error) {
+	secret, err := c.Logical().List(path)
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+// ReadSecret requests the secret/data from Vault
+// c : Vault client
+// key : the key for the desired secret/data
+func ReadSecret(c *api.Client, key string) (*api.Secret, error) {
+	secret, err := c.Logical().Read(key)
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+// WriteSecret adds a secret to Vault
+// c : Vault client
+// key : the key for the secret
+// data : config for use when writing data
+func WriteSecret(c *api.Client, key string, data map[string]interface{}) (bool, error) {
+	if _, err := c.Logical().Write(key, data); err != nil {
+		return false, err
+	}
+	return true, nil
+}
